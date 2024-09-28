92047e9084113eea6cff26c35907df3acaabb669
chrissysemens <chrissysemens@gmail.com>
2024-09-28 23:11:38 +0100

    Ask itself

--- api/index.ts
+++ api/index.ts
@@ -12,9 +12,11 @@ const chat = new ChatOpenAI({
 
 app.get("/test", async (req, res) => {
     try {
-        await chat.invoke([new HumanMessage({ content: "Generate a random thought provoking question" })]).then((resp: any) => {
-            res.send(resp);
-        })
+        await chat.invoke([new HumanMessage({ content: "Generate a random thought provoking question" })]).then(async (resp: any) => {
+            await chat.invoke([new HumanMessage({ content: resp.content })]).then((resp: any) => {
+                res.send(resp);
+            })
+        });
     } catch(e) {
         res.send(e);
     }
