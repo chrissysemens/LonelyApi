2f23092930d014529532f1a96cafc6c0a44fcaae
chrissysemens <chrissysemens@gmail.com>
2024-09-28 22:16:29 +0100

    Testing Api Key

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 const express = require("express"); const app = express();
 require('dotenv').config();
 
-app.get("/test", (req, res) => res.send("Express on Vercel"));
+app.get("/test", (req, res) => res.send(process.env.OPENAI_API_KEY));
 
-app.listen(3000, () => console.log(process.env.OPENAI_API_KEY));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
